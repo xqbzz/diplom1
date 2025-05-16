e86d5cfe81ec6932496e028f51a10710300d709b
sergeybaranov <se.bar2300@mail.ru>
2025-05-16 14:40:55 +0300

    Обновил main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import sys
-sys.setrecursionlimit(5000)
-from PyQt5.QtWidgets import QApplication
-from gui import ImageGeneratorApp
 import os
 import PyQt5
 
-# Получаем папку, где установлен PyQt5
 pyqt_path = os.path.dirname(PyQt5.__file__)
-plugin_path = os.path.join(pyqt_path, "Qt5", "plugins")
+possible_plugin_paths = [
+    os.path.join(pyqt_path, "Qt5", "plugins"),
+    os.path.join(pyqt_path, "Qt", "plugins"),
+]
 
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+plugin_path = None
+for path in possible_plugin_paths:
+    if os.path.exists(os.path.join(path, "platforms", "qwindows.dll")):
+        plugin_path = path
+        break
+
+if plugin_path:
+    os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
+    print(f"✅ Используем путь к плагинам: {plugin_path}")
+else:
+    print("❌ Плагин платформы не найден. Убедись, что файл qwindows.dll существует.")
+    sys.exit(1)  # Завершаем работу, чтобы не запускать GUI без плагинов
+
+from PyQt5.QtWidgets import QApplication
+from gui import ImageGeneratorApp
+
+sys.setrecursionlimit(5000)
 
 def main():
     app = QApplication(sys.argv)
